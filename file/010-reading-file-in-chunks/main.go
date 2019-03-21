e1730c5a303c968c5b9ee9d4142baddbbcbe87c4
Wang, Hui <wanghui@paratera.com>
2019-03-21 11:19:51 +0800

    add read file in chunks method

---
+++ file/010-reading-file-in-chunks/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	const BufferSize = 100
+	file, err := os.Open("file.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	buffer := make([]byte, BufferSize)
+
+	for {
+		bytesread, err := file.Read(buffer)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println(err)
+			}
+			break
+		}
+
+		fmt.Println("bytes read: ", bytesread)
+		fmt.Println("bytestream to string: ", string(buffer[:bytesread]))
+	}
+}
