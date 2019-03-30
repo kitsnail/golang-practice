fe318ae0012d40994fd3e81cf8467bdb77b79cd2
Wang, Hui <wanghui@paratera.com>
2019-03-30 23:20:34 +0800

    add pipline v1

---
+++ golang-base/goroutine/pipeline1/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	naturals := make(chan int)
+	squares := make(chan int)
+
+	// counter
+	go func() {
+		for x := 0; ; x++ {
+			naturals <- x
+		}
+	}()
+
+	// squares
+	go func() {
+		for {
+			x, ok := <-naturals
+			if !ok {
+				break
+			}
+			squares <- x * x
+		}
+		close(squares)
+	}()
+
+	//  printer
+
+	for {
+		fmt.Println(<-squares)
+		time.Sleep(1000 * time.Millisecond)
+	}
+
+}
