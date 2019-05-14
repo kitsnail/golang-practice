4e2750ce43ef21eb7df66766685a684936b14bef
Wang, Hui <wanghui@paratera.com>
2019-05-14 11:07:55 +0800

    add net dialtimeout

---
+++ net/dial-timeout1/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatalln("please give a server address")
+	}
+
+	checkServerPort(os.Args[1])
+}
+
+func checkServerPort(addr string) {
+	timeout := time.Duration(5 * time.Second)
+	t1 := time.Now()
+	_, err := net.DialTimeout("ip4:1", addr, timeout)
+	fmt.Println("wast time:", time.Now().Sub(t1))
+	if err != nil {
+		log.Println("site unreachable, error:", err)
+		return
+	}
+	fmt.Println("tcp server is ok")
+}
